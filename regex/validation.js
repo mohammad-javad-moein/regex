689759f527a290c38a1acb38da8a146e4d7bd8d5
mohammad-javad-moein <107869660+mohammad-javad-moein@users.noreply.github.com>
2022-06-21 14:11:01 +0430

    Add files via upload

---
+++ regex/validation.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\d\w@-]{8,20}$/i,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+};
+
+// validation function
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
